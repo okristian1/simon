5a4f5efc30001620a3a2b151b1d52289ddcbb5a1
locrin <okristian1@gmail.com>
2017-04-04 22:19:10 +0200

    sounds

---
+++ main.js
@@ -0,0 +1,25 @@
+var red = document.getElementById("red");
+var yellow = document.getElementById("yellow");
+var blue = document.getElementById("blue");
+var green = document.getElementById("green");
+var redAudio = document.getElementById("redAudio");
+
+
+red.onclick = function() {
+      redAudio.play()
+  }
+yellow.onclick = function() {
+      yellowAudio.play()
+  }
+blue.onclick = function() {
+      blueAudio.play()
+  }
+green.onclick = function() {
+  greenAudio.play()
+  }
+
+
+  // function PlaySound() {
+  //       var sound = document.getElementById("audio");
+  //       sound.play()
+  //   }
