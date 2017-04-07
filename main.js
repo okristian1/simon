eaa810315ffad8539eaec3970ac27bb470ce6895
locrin <okristian1@gmail.com>
2017-04-07 13:38:16 +0200

    game loop kinda works

--- main.js
+++ main.js
@@ -3,32 +3,42 @@ var yellow = document.getElementById("yellow");
 var blue = document.getElementById("blue");
 var green = document.getElementById("green");
 var circles = document.querySelectorAll('.circle');
+var info = document.querySelector('.info');
 var colors = ['red', 'yellow', 'green', 'blue'];
 
-var sequenceCurrent = ['red', 'yellow', 'green', 'blue', 'green', 'blue', 'red']; //filled for testing
+var sequenceCurrent = ['red', 'green', 'blue', 'yellow']; //filled for testing
 
 //make circle clicked compare to sequence not circle incremented.
 
 window.onload = function() {
+
+function gameLoop() {
   var j = 0;
   for (var i = 0; i < circles.length; i++) {
       circles[i].onclick = function(e) {
         var circle = this.value;
-        for (var i = 0; i < sequenceCurrent.length; i++) {
-          console.log(circle, sequenceCurrent[j]);
-          if (circle === sequenceCurrent[j]) {
-            console.log("ayyy");
+        for (var i = 0; i < sequenceCurrent.length ; i++) {
+          console.log(j, sequenceCurrent.length, circle, sequenceCurrent[j]);
+          console.log(sequenceCurrent);
+          if (j === sequenceCurrent.length-1) {
+            j=0;
+            sequenceAdd();
+            console.log(sequenceCurrent);
+            break;
+          } else if (circle !== sequenceCurrent[j]) {
+            j=0;
+            info.innerHTML = "Try again";
+            break;
+          } else {
             j++;
             break;
           }
-          else {
-            console.log("boooo");
-             break;
-          }
         }
+      }
     }
   }
-}
+
+sequenceCurrent.length
 
 function sequenceAdd() {
   var color = colors[Math.floor(Math.random()*colors.length)];
@@ -73,3 +83,7 @@ blue.onclick = function() {
 green.onclick = function() {
   greenAudio.play()
   }
+
+gameLoop();
+
+}
